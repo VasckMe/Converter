73f62fa5d5804c7bfd532b3cb36a23ac8d1087db
Anton Kasaryn <110229952+VasckMe@users.noreply.github.com>
2024-07-07 17:01:49 +0200

    Update install resources file

--- installResources.sh
+++ installResources.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# MacOS
-
-# Create private environment
-python3 -m venv myenv
-
-# Activate your environment
-
-source myenv/bin/activate
-
-# Install all required frameworks
-
-pip3 install xmltodict
-pip3 install pyyaml
+pip install xmltodict
+pip install pyyaml
+pip install pyinstaller
