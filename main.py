304abb038338017a84d7f5b73d318ea65fc9a9cb
Anton <kasarin.anton@gmail.com>
2024-07-07 15:49:29 +0200

    Add arguments parsing

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import argparse
+
+# Parsing
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Date Processing')
+    parser.add_argument('input', type=str, help='Input filepath')
+    parser.add_argument('output', type=str, help='Output filepath')
+    args = parser.parse_args()
+    return args
+
+
 def main():
     print("-====    Main function    ====-")
 
