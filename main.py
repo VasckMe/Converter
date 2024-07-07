6997b65a5418ab333aca179e67b37c6ee903b396
Anton <kasarin.anton@gmail.com>
2024-07-07 15:54:07 +0200

    Add saves for json, xml, yml

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
+import json
+import xmltodict
+import yaml
+
+def save_json(data, filepath):
+    with open(filepath, 'w') as file:
+        json.dump(data, file, indent=4)
+    print(f'Dane zapisane do pliku: {filepath}')
+
+def save_xml(data, filepath):
+    with open(filepath, 'w') as file:
+        xml_str = xmltodict.unparse(data, pretty=True)
+        file.write(xml_str)
+    print(f'Dane zapisane do pliku: {filepath}')
+
+def save_yaml(data, filepath):
+    with open(filepath, 'w') as file:
+        yaml.dump(data, file)
+    print(f'Dane zapisane do pliku: {filepath}')
+
+def save_file(data, filepath):
+    if filepath.endswith('.json'):
+        save_json(data, filepath)
+    elif filepath.endswith('.xml'):
+        save_xml(data, filepath)
+    elif filepath.endswith('.yaml') or filepath.endswith('.yml'):
+        save_yaml(data, filepath)
+    else:
+        raise ValueError('Nieobsługiwany format pliku.')
+
 def main():
     print("-====    Main function    ====-")
 
