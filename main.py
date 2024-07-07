6cca8d51614b414e3f52d10e086a64415b0a0957
Anton <kasarin.anton@gmail.com>
2024-07-07 15:51:27 +0200

    Add loads for json, xml, yml

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
+import json
+import xmltodict
+import yaml
+
+# Loads
+
+# Load type handler
+def load_file(filepath):
+    if filepath.endswith('.json'):
+        return load_json(filepath)
+    elif filepath.endswith('.xml'):
+        return load_xml(filepath)
+    elif filepath.endswith('.yaml') or filepath.endswith('.yml'):
+        return load_yaml(filepath)
+    else:
+        raise ValueError('Nieobsługiwany format pliku.')
+
+# json
+def load_json(filepath):
+    with open(filepath, 'r') as file:
+        data = json.load(file)
+    return data
+
+# xml
+def load_xml(filepath):
+    with open(filepath, 'r') as file:
+        data = xmltodict.parse(file.read())
+    return data
+
+# yaml
+def load_yaml(filepath):
+    with open(filepath, 'r') as file:
+        data = yaml.safe_load(file)
+    return data
+
 def main():
     print("-====    Main function    ====-")
 
