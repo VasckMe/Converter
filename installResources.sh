41b26d62a5facb6e0b3f7953d68fb5ee97996add
Anton <kasarin.anton@gmail.com>
2024-07-07 16:00:05 +0200

    MacOS: Add creating environment Add installing of required dependencies

--- installResources.sh
+++ installResources.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# MacOS
+
+# Create private environment
+python3 -m venv myenv
+
+# Activate your environment
+
+source myenv/bin/activate
+
+# Install all required frameworks
+
+pip3 install xmltodict
+pip3 install pyyaml
